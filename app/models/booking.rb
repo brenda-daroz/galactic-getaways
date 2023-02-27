f625c55eff8b645f3774c25a4b16bb52c10db9d3
Jan Dachlauer <jandachlauer@gmail.com>
2023-02-27 17:21:25 +0100

    Added validations,created database, added description to Spaceships and migrated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :spaceship
+
+  validates :spaceship, :user, presence: true
+  # start and end date overlap?
+
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,4 +1,9 @@
 class Spaceship < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, uniqueness: true
+  validates :user, :photo_url, presence: true
+  validates :description, length: { minimum: 10}
+
 end
