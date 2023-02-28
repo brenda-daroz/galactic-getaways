f02574cbd4aac6ca2f7f7bb5d561fb9884c325d5
Frank Jurettko <f.jurettko@gmx.de>
2023-02-28 15:01:44 +0100

    new_create_view_spaceship_model

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -7,4 +7,16 @@ class SpaceshipsController < ApplicationController
   def new
     @spaceship = Spaceship.new
   end
+
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    @spaceship.save
+  end
+
+  private
+
+def spaceship_params
+  params.require(:spaceship).permit(:name, :description, :price, :photos: [])
+end
+
 end
