b7000d7998410c69b21b5bc3dff6e91a7e557c40
Jan Dachlauer <jandachlauer@gmail.com>
2023-02-28 11:43:29 +0100

    Completed seed and adjusted validation and association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
+User.destroy_all
 Spaceship.destroy_all
-# User.destroy_all
 
+puts 'Creating 5 fake users...'
 5.times do
   User.create(
   email: "#{Faker::Name.first_name}@gmail.com",
-  password: "123455"
+  password: "123456"
 )
 end
 puts 'Creating 10 fake spaceships...'
@@ -26,4 +26,47 @@ puts 'Creating 10 fake spaceships...'
   )
   spaceship.save!
 end
+
+puts 'Creating 7 fake bookings...'
+
+7.times do
+  booking = Booking.new(
+    spaceship_id: Spaceship.all.sample.id,
+    user_id: User.all.sample.id,
+    start_date: Faker::Date.between(from: '2023-03-01', to: '2023-12-01'),
+  )
+  booking.end_date = Faker::Date.between(from: booking.start_date, to: '2023-12-01')
+  booking.save!
+end
+
 puts 'Finished!'
+
+
+# Alternate version made by Sebi
+# 4.times do
+#   user = User.new(
+#     email: "#{Faker::Name.first_name}@gmail.com",
+#     password: "123456"
+#   )
+#   user.save!
+#   10.times do
+#     spaceship = Spaceship.new(
+#       name:    Faker::Space.nasa_space_craft,
+#       price: "#{Faker::Number.decimal(l_digits: 2)} $",
+#       description: Faker::Quote.yoda
+#     )
+#     spaceship.user = user
+#     spaceship.save!
+#   end
+# end
+
+# 7.times do
+#   booking = Booking.new(
+#     spaceship_id: Spaceship.all.sample.id,
+#     user_id: User.all.sample.id,
+#     start_date: Faker::Date.between(from: '2023-03-01', to: '2023-12-01'),
+#   )
+#   # puts 'made a booking without start date'
+#   booking.end_date = Faker::Date.between(from: booking.start_date, to: '2023-12-01')
+#   booking.save!
+# end
