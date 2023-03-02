1cadb06b10621147477b3f9e1a76d77c384a39c5
Brenda Daroz <brenda.daroz@gmail.com>
2023-03-02 17:23:09 +0100

    fix show page, footer, index

--- db/schema.rb
+++ db/schema.rb
@@ -60,12 +60,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_110441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "address"
     t.integer "seats"
     t.integer "power"
     t.integer "speed"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_spaceships_on_user_id"
   end
 
